a7a99748f34a9fb7ea79100e2760c82b5652a7dc
Davide <davide@hibernate.org>
2022-06-10 09:56:48 +0100

    Test case

--- src/test/java/org/acme/hibernate/orm/panache/FruitsEndpointTest.java
+++ src/test/java/org/acme/hibernate/orm/panache/FruitsEndpointTest.java
@@ -14,57 +14,7 @@ import static org.hamcrest.text.IsEmptyString.emptyString;
 public class FruitsEndpointTest {
 
     @Test
-    public void testListAllFruits() {
-        //List all, should have all 3 fruits the database has initially:
-        Response response = given()
-                .when()
-                .get("/fruits")
-                .then()
-                .statusCode(200)
-                .contentType("application/json")
-                .extract().response();
-        assertThat(response.jsonPath().getList("name")).containsExactlyInAnyOrder("Cherry", "Apple", "Banana");
-
-        // Update Cherry to Pineapple
-        given()
-                .when()
-                .body("{\"name\" : \"Pineapple\"}")
-                .contentType("application/json")
-                .put("/fruits/1")
-                .then()
-                .statusCode(200)
-                .body(
-                        containsString("\"id\":"),
-                        containsString("\"name\":\"Pineapple\""));
-
-        //List all, Pineapple should've replaced Cherry:
-        response = given()
-                .when()
-                .get("/fruits")
-                .then()
-                .statusCode(200)
-                .contentType("application/json")
-                .extract().response();
-        assertThat(response.jsonPath().getList("name"))
-                .containsExactlyInAnyOrder("Pineapple", "Apple", "Banana");
-
-        //Delete Pineapple:
-        given()
-                .when()
-                .delete("/fruits/1")
-                .then()
-                .statusCode(204);
-
-        response = given()
-                .when()
-                .get("/fruits")
-                .then()
-                .statusCode(200)
-                .contentType("application/json")
-                .extract().response();
-        assertThat(response.jsonPath().getList("name"))
-                .containsExactlyInAnyOrder("Apple", "Banana");
-
+    public void testInsertNewFruit() {
         //Create the Pear:
         given()
                 .when()
@@ -76,37 +26,6 @@ public class FruitsEndpointTest {
                 .body(
                         containsString("\"id\":"),
                         containsString("\"name\":\"Pear\""));
-
-        //List all, Pineapple should be still missing now:
-        response = given()
-                .when()
-                .get("/fruits")
-                .then()
-                .statusCode(200)
-                .extract().response();
-        assertThat(response.jsonPath().getList("name"))
-                .containsExactlyInAnyOrder("Pear", "Apple", "Banana");
     }
 
-    @Test
-    public void testEntityNotFoundForDelete() {
-        given()
-                .when()
-                .delete("/fruits/9236")
-                .then()
-                .statusCode(404)
-                .body(emptyString());
-    }
-
-    @Test
-    public void testEntityNotFoundForUpdate() {
-        given()
-                .when()
-                .body("{\"name\" : \"Watermelon\"}")
-                .contentType("application/json")
-                .put("/fruits/32432")
-                .then()
-                .statusCode(404)
-                .body(emptyString());
-    }
 }
